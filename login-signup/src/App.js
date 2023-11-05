d14a97ea1f845b65b1fba077ccdb28961ffb43da
kavyansh <kavyansh21sharma@gmail.com>
2023-11-05 15:13:23 +0530

    initial commit

---
+++ login-signup/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import LoginSignup from "./components/loginsignup/loginsinup"
+
+
+function App() {
+  return (
+    <div >
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
